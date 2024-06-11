2ee1b7ad2e152c4b6e31de052dbcb8ae5adc191e
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-06-11 11:13:46 +0100

    generate token

--- widjit-paiement-app/src/app/components/token/steps/steps.component.ts
+++ widjit-paiement-app/src/app/components/token/steps/steps.component.ts
@@ -1,51 +1,93 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'; 
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatButtonModule} from '@angular/material/button';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DataService } from '../../../service/data.service';
+import { TokenService } from '../../../service/token.service';
 
 @Component({
   selector: 'app-steps',
   templateUrl: './steps.component.html',
-  styleUrls: ['./steps.component.css'] 
+  styleUrls: ['./steps.component.css'],
 })
 export class StepsComponent implements OnInit {
+  nameForm: FormGroup | any;
+  emailForm: FormGroup | any;
+  isNameEditable: boolean = true;
+  token!: string;
 
-  nameForm: FormGroup | any; 
-  emailForm: FormGroup| any; 
-
-  constructor(private _formBuilder: FormBuilder) { } 
+  constructor(
+    private _formBuilder: FormBuilder,
+    private tokenService: TokenService,
+    public dataService: DataService
+  ) {}
 
   ngOnInit() {
-    this.initializeForms(); 
+    this.initializeForms();
   }
 
   initializeForms() {
     // Initialize name form with validation
     this.nameForm = this._formBuilder.group({
-      firstName: ['', Validators.required] // First name field is required
+      firstName: ['', Validators.required], // First name field is required
     });
 
     // Initialize email form with validation
     this.emailForm = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]] 
+      email: ['', [Validators.required, Validators.email]],
     });
   }
 
-  ////////
-  isNameEditable: boolean = true;
-
-  // makeNameEditable() {
-  // this.isNameEditable = true;
-  // }
   makeNameNoEditable() {
     this.isNameEditable = false;
   }
 
-
-  ///////////////
   reloadPage() {
     window.location.reload();
+  } /*
+  generateToken() {
+    const name = this.nameForm.get('firstName')?.value || ''; // Get name from form
+    const email = this.emailForm.get('email')?.value || ''; // Get email from form
+    const orderId = '4'; // Get orderId from DataService
+    const orderAmount = 445; // Get orderAmount from DataService
+    const currency = 'MAD'; // Get currency from DataService
+    const marchandId = 4; // Get marchandId from DataService
+
+    this.tokenService
+      .generateToken(
+        orderId,
+        orderAmount.toString(),
+        name,
+        email,
+        currency,
+        marchandId.toString()
+      )
+      .subscribe(
+        (token) => {
+          (this.token = token), console.log('Token:', token);
+        },
+        (error) => {
+          console.log('token error');
+        }
+      );
+  }*/
+
+  
+  generateToken() {
+    const name = this.nameForm.get('firstName')?.value || ''; // Get name from form
+    const email = this.emailForm.get('email')?.value || ''; // Get email from form
+    const orderId = this.dataService.orderId || ''; // Get orderId from DataService
+    const orderAmount = this.dataService.amount || ''; // Get orderAmount from DataService
+    const currency = this.dataService.currency || ''; // Get currency from DataService
+    const marchandId = this.dataService.marchandId || ''; // Get marchandId from DataService
+
+
+    this.tokenService.generateToken(orderId, orderAmount.toString(), name, email, currency, marchandId.toString()).subscribe(
+      token => {
+        this.token=token,
+        console.log('Token:', token); 
+      },
+      error => {
+        console.log('token error'); 
+      }
+    );
   }
 }
---
+++ widjit-paiement-app/src/app/service/token.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+  private baseUrl = 'http://localhost:8080/token';
+
+  constructor(private http: HttpClient) {}
+
+  generateToken(
+    orderId: string,
+    orderAmount: string,
+    customerName: string,
+    customerMail: string,
+    currency: string,
+    marchandId: string
+  ) {
+    const url = `${this.baseUrl}/generate-token`;
+    const params = {
+      orderId: orderId,
+      orderAmount: orderAmount,
+      customerMail: customerMail,
+      customerName: customerName,
+      currency: currency,
+      marchandId: marchandId,
+    };
+
+    return this.http.get<string>(url, { params });
+  }
+}
